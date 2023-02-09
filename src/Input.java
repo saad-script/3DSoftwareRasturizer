09df7131f7707b08680574b1f2b30dc1745141c9
Ahmed Khan <saadsword@gmail.com>
2023-02-09 15:18:36 -0500

    Input system cleanup

--- src/Input.java
+++ src/Input.java
@@ -1,7 +1,11 @@
+import javafx.event.EventHandler;
 import javafx.geometry.Point2D;
+import javafx.scene.Cursor;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.input.MouseEvent;
+
 
 import java.awt.*;
 import java.util.HashSet;
@@ -14,41 +18,20 @@ public class Input {
     private static final Vector3 prevMousePos = new Vector3(0, 0);
 
 
-    public static void initializeInput(SceneRenderer3D sceneRenderer) {
-        Main.windowScene.setOnKeyPressed(null);
-        Main.windowScene.setOnKeyReleased(null);
-
-        Main.windowScene.setOnKeyPressed(key -> {
-            if(key.getCode() == KeyCode.W
-                    || key.getCode() == KeyCode.A
-                    || key.getCode() == KeyCode.S
-                    || key.getCode() == KeyCode.D) {
-                keysDown.add(key.getCode());
-            }
+    public Input(Canvas canvas) {
+        canvas.setOnMousePressed(event -> {
+            canvas.requestFocus();
+            mouseDown.add(event.getButton());
         });
-
-        Main.windowScene.setOnKeyReleased(key -> {
-            if(key.getCode() == KeyCode.W
-                    || key.getCode() == KeyCode.A
-                    || key.getCode() == KeyCode.S
-                    || key.getCode() == KeyCode.D) {
-                keysDown.remove(key.getCode());
-            }
-        });
-
-        sceneRenderer.setOnMousePressed(mouse -> {
-            mouseDown.add(mouse.getButton());
-        });
-
-        sceneRenderer.setOnMouseReleased(mouse -> {
-            mouseDown.remove(mouse.getButton());
-        });
+        canvas.setOnMouseReleased(event -> mouseDown.remove(event.getButton()));
+        canvas.setOnKeyPressed(key -> keysDown.add(key.getCode()));
+        canvas.setOnKeyReleased(key -> keysDown.remove(key.getCode()));
     }
 
 
-    public static void pollInput(SceneRenderer3D sceneRenderer) {
+    public void pollInput(Canvas canvas) {
         Point mouseScreenPos = MouseInfo.getPointerInfo().getLocation();
-        Point2D mouseLocalPos = sceneRenderer.screenToLocal(mouseScreenPos.getX(), mouseScreenPos.getY());
+        Point2D mouseLocalPos = canvas.screenToLocal(mouseScreenPos.getX(), mouseScreenPos.getY());
 
         prevMousePos.setX(currMousePos.getX());
         prevMousePos.setY(currMousePos.getY());
@@ -57,19 +40,19 @@ public class Input {
         currMousePos.setY(mouseLocalPos.getY());
     }
 
-    public static boolean keyIsDown(KeyCode key) {
+    public boolean keyIsDown(KeyCode key) {
         return keysDown.contains(key);
     }
 
-    public static boolean mouseIsDown(MouseButton mouseButton) {
+    public boolean mouseIsDown(MouseButton mouseButton) {
         return mouseDown.contains(mouseButton);
     }
 
-    public static Vector3 getMousePosition() {
+    public Vector3 getMousePosition() {
         return currMousePos;
     }
 
-    public static Vector3 getMouseDelta() {
+    public Vector3 getMouseDelta() {
         return currMousePos.subtract(prevMousePos);
     }
 
